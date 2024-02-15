3b24293b252a4652587e22da311f3ac9e9d65857
Minh <minhbh2000@gmail.com>
2024-02-15 09:41:28 +0700

    create controller

--- src/main/java/fa/edu/controller/pageController.java
+++ src/main/java/fa/edu/controller/pageController.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-public class claimController {
+public class pageController {
     @Autowired
     StaffRepository staffRepository;
-    @GetMapping("/claim/draft/{id}")
-    public String DraftClaim(@PathVariable("id") Integer id) {
+    @GetMapping("/claim/create/{id}")
+    public String homePageClaim(@PathVariable("id") Integer id) {
         staffRepository.findById(id);
-        return "DraftClaim";
+        return "createClaim";
+    }
+    @GetMapping("/claim/edit/{id}")
+    public String EditClaim(@PathVariable("id") Integer id) {
+        staffRepository.findById(id);
+        return "EditClaim";
     }
 }
